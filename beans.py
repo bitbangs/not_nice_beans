b4c6fbd17a02b77d8a45a46fc416ff407cdaf87b
bitbangs <rickchristo11@gmail.com>
2019-08-23 16:58:10 -0400

    broken:drop beans not found in settle_coords

--- beans.py
+++ beans.py
@@ -101,8 +101,9 @@ class SettledBeans:
 
     def SettleDetect(self, bean):
         settle_coordinates = self.GenSettleCoords()
-        if settle_coordinates.count(bean.coordinate):
+        @@@if settle_coordinates.count(bean.coordinate): #drop beans not in settle coordinates!!
             bean.has_settled = True
+            logger.info('settled %s', bean)
             return True
         return False
 
@@ -166,11 +167,8 @@ class SettledBeans:
                             #explore problem when we drop bean of same color
                             del self.color_map[coordinate]
                             self.match_graph.RemoveVertex(coordinate)
-                            #reduce settle height and create drop bean
+                            #reduce settle height
                             self.column_heights[coordinate[0]] = self.column_heights[coordinate[0]] - 1
-                            north_coordinate = (coordinate[0], coordinate[1] - 1)
-                            if north_coordinate in self.color_map and drop_color != self.color_map[north_coordinate]: #what happens when the lookup fails? can we jsut get(north_coord)?
-                                logger.info('dropbean spawned because %s:%s is north of %s in color map', north_coordinate, coordinate, drop_color)
                     return True
             return False
 
@@ -192,4 +190,6 @@ class SettledBeans:
                     drop_bean = DroppingBean(self.color_map[coordinate], coordinate)
                     del self.color_map[coordinate]
                     dropping_beans.append(drop_bean)
+        for bean in dropping_beans:
+            self.match_graph.RemoveVertex(bean.coordinate)
         return dropping_beans
